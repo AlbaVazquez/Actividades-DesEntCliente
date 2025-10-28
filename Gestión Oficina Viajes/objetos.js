7fe45eb2cfa98a8dc79e752df5aefa7465b7eb43
AlbaVazquez <xavazgui498@ieshnosmachado.org>
2025-10-28 13:55:08 +0100

    .

---
+++ Gestión Oficina Viajes/objetos.js
@@ -0,0 +1,228 @@
+class Alojamiento {
+    _idALojamiento;
+    _numPersonas;
+    
+    constructor(idALojamiento, numPersonas) {
+        this._idALojamiento = idALojamiento;
+        this._numPersonas = numPersonas;
+    }
+
+    get idALojamiento() {
+        return this._idALojamiento;
+    }
+    set idALojamiento(value) {
+        this._idALojamiento = value;
+    }
+
+    get numPersonas() {
+        return this._numPersonas;
+    }
+    set numPersonas(value) {
+        this._numPersonas = value;
+    }
+    
+    toHTMLRow() {
+        let fila = "<tr>"; //Crear la primera fila
+        for(let atributo of Object.values(this)) { //Crear un bucle para recorrer los atributos del objeto
+            fila += "<td>" + atributo + "</td>"; //Añadir cada atributo a la fila con una celda
+        }
+        return fila + "</tr>"; //Cerrar la fila y devolverla
+    }
+}
+
+
+class Habitacion extends Alojamiento {
+    _desayuno;
+
+    constructor(desayuno) {
+        super(idAlojamiento, numPersonas);
+        this._desayuno = desayuno;
+    }
+
+    get desayuno() {
+        return this._desayuno;
+    }
+    set desayuno(value) {
+        this._desayuno = value;
+    }
+    
+}
+
+
+class Apartamento extends Alojamiento {
+    _parking;
+    _dormitorios;
+
+    constructor(parking, dormitorios) {
+        super(idAlojamiento, numPersonas);
+        this._parking = parking;
+        this._dormitorios = dormitorios;
+    }
+
+    get dormitorios() {
+        return this._dormitorios;
+    }
+    set dormitorios(value) {
+        this._dormitorios = value;
+    }
+
+    get parking() {
+        return this._parking;
+    }
+    set parking(value) {
+        this._parking = value;
+    }
+}
+
+
+class Cliente {
+    _dniCLiente;
+    _nombre;
+    _apellidos;
+    _usuario;
+
+    constructor(dniCLiente, nombre, apellidos, usuario) {
+        this._dniCLiente = dniCLiente;
+        this._nombre = nombre;
+        this._apellidos = apellidos;
+        this._usuario = usuario;
+    }
+
+    get nombre() {
+        return this._nombre;
+    }
+    set nombre(value) {
+        this._nombre = value;
+    }
+
+    get usuario() {
+        return this._usuario;
+    }
+    set usuario(value) {
+        this._usuario = value;
+    }
+
+    get apellidos() {
+        return this._apellidos;
+    }
+    set apellidos(value) {
+        this._apellidos = value;
+    }
+
+    get dniCLiente() {
+        return this._dniCLiente;
+    }
+    set dniCLiente(value) {
+        this._dniCLiente = value;
+    }
+}
+
+class Reserva {
+    _idReserva;
+    _cliente;
+    _alojamiento;
+    _fechaInicio;
+    _fechaFin;
+
+    constructor(idReserva, cliente, alojamiento, fechaInicio, fechaFin) {
+        this._idReserva = idReserva;
+        this._cliente = cliente;
+        this._alojamiento = alojamiento;
+        this._fechaInicio = fechaInicio;
+        this._fechaFin = fechaFin;
+    }
+
+    get fechaFin() {
+        return this._fechaFin;
+    }
+    set fechaFin(value) {
+        this._fechaFin = value;
+    }
+
+    get fechaInicio() {
+        return this._fechaInicio;
+    }
+    set fechaInicio(value) {
+        this._fechaInicio = value;
+    }
+
+    get alojamiento() {
+        return this._alojamiento;
+    }
+    set alojamiento(value) {
+        this._alojamiento = value;
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+    set cliente(value) {
+        this._cliente = value;  
+    }
+}
+
+
+class Agencia {
+    _clientes = [];
+    _reservas = [];
+    _alojamientos = [];
+
+    get alojamientos() {
+        return this._alojamientos;
+    }
+    set alojamientos(value) {
+        this._alojamientos = value;
+    }
+
+    get reservas() {
+        return this._reservas;
+    }
+    set reservas(value) {
+        this._reservas = value;
+    }
+
+    get clientes() {
+        return this._clientes;
+    }
+    set clientes(value) {
+        this._clientes = value;
+    }
+
+    altaCLiente(oCLiente){
+
+    }
+
+    altaAlojamiento(oAlojamiento){
+
+    }
+    
+    altaReserva(oReserva){
+
+    }
+
+    bajaReserva(idReserva){
+
+    }
+
+    listadoClientes(){
+
+    }
+
+    listadoAlojamientos(){
+
+    }
+
+    listadoReservas(fechaInicio, fechaFin){
+
+    }
+
+    listadoReservasCliente(dniCliente){
+        
+    }   
+
+    listadoHabitacionesConDesayuno(){
+
+    }
+}
+
+
